48e8fea8fc67c22f2e5cecbfb67890583dcde841
Leonardo Bruksch <leonardokbruksch@hotmail.com>
2018-11-28 20:28:01 -0200

    informations on read me about application

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # testeZallpy
+
+Teste prático para a Zallpy.
+
+Aplicação web para listagem de usuários.
+
+Realizado utilizando:
+Java 8, Spring Boot, Maven, JPA, HSQLDB, HTML5 e JQUERY.
+
+Passo a passo para importação:
+O projeto foi desenvolvido utilizando o IntelliJ com o Maven para gerenciamento das dependências.
+Para testar a aplicação faça o download deste repositório e o importe para o IntelliJ na opção "Import Project".
+O maven irá realizar automaticamente o download das dependências.
+ 
+Execute a aplicação pelo intelliJ e acesse em seu browser o localhost: http://localhost:8080/
+
+Sobre a aplicação:
+
+O seu backend foi desenvolvido utilizando Java.
+Foi utilizado o SpringBoot como framework MVC por ser um framework fácil e rápido de configurar e começar o desenvolvimento.
+Foi utilizado o Maven para gerenciar as dependências e descartar a necessidade de importá-las manualmente.
+Para criar as entidades foi utilizado o JPA, o qual facilita a comunicação com o banco de dados.
+Para o banco de dados foi utilizado o HSQLDB.
+
+O frontend foi desenvolvido utilizando HTML5 e Javascript (Jquery).
+Foi utilizado Bootstrap para tornar o site responsivo.
+Para a criação da tabela com os dados dos usuários, foi utilizado uma biblioteca javascript chamada Bootstrap-Table.
+
+Pontos forte:
+A aplicação é altamente customízável e pode ser desenvolvida rapidamente.
+
+Pontos de melhoria:
+Dependência com a biblioteca Bootstrap-Table. Caso seja descontinuada será necessário alterar a criação das tabelas.
--- src/main/resources/static/js/users.js
+++ src/main/resources/static/js/users.js
@@ -2,43 +2,7 @@ $(document).ready(function () {
 
     createBootstrapTable();
 
-    $("#submitUpload").click(function (event) {
-        event.preventDefault();
-        fireAjaxFileUpload();
-    });
-
-    $("#submitClassSelection").click(function (event) {
-        event.preventDefault();
-        setSelectedClasses();
-    });
-
 });
-
-function fireAjaxFileUpload() {
-
-    var form = $('#fileUploadForm')[0];
-    var data = new FormData(form);
-
-    $.ajax({
-        type: "POST",
-        enctype: 'multipart/form-data',
-        url: "upload",
-        data: data,
-        processData: false,
-        contentType: false,
-        cache: false,
-        timeout: 600000,
-        datatype: "json",
-        success: function (data) {
-
-            $('#fileUploadForm').hide();
-
-            createBootstrapTable();
-
-        }
-    });
-
-}
 
 function createBootstrapTable(){
 
@@ -82,35 +46,4 @@ function createBootstrapTable(){
 
     $('#usersContainer').show();
 
-
-}
-
-function setSelectedClasses() {
-
-    activeClasses = getSelectedClasses();
-
-    $.ajax( {
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        type: 'POST',
-        url: '/setSelectedClasses',
-        data: JSON.stringify(activeClasses),
-        datatype: "json",
-        success: function(data) {
-
-            $('#classesSelectionContainer').hide();
-
-            createClassesData(data);
-            $('#classesCards').find('.carousel-item').first().addClass('active');
-            $('#classesContainer').show();
-        }
-    });
-
-}
-
-function getSelectedClasses(){
-    selectedClasses = $('#selectClassesTable').bootstrapTable('getSelections');
-    return selectedClasses;
 }
